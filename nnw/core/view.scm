aec6e0f5812d8f3a739dacaed2d99de069e98af2
MinkieYume <sign@yumieko.com>
2026-01-01 20:36:57 +0800

    feat: add view module to core library

---
+++ nnw/core/view.scm
@@ -0,0 +1,24 @@
+(define-module (nnw core view)
+  #:use-module (oop goops)
+  #:use-module (uuid generate)
+  #:export (<view>
+	    view-id
+	    view-name
+	    view-metadata
+	    view-content
+	    view->string))
+
+(define-class <view> ()
+  (id #:init-keyword #:id
+      #:init-thunk generate-string-uuid
+      #:getter view-id)
+  (name #:init-keyword #:description #:getter view-name)
+  (metadata #:init-keyword #:metadata #:init-value '() #:getter view-metadata)
+  (content #:init-keyword #:content #:init-value '() #:getter view-content))
+
+;; Format a view to a string
+(define-method (view->string (view <view>))
+  (view-name view))
+
+(define-method (initialize (view <view>) . initargs)
+  (next-method))
