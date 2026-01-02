1b57770d1987f85d3f0c5cc944843892e9520f34
MinkieYume <sign@yumieko.com>
2026-01-02 14:15:06 +0800

    fix: correct module path and add document view implementation

--- nnw/core/view/document.scm
+++ nnw/core/view/document.scm
@@ -1,7 +1,9 @@
-(define-module (nnw core document)
+(define-module (nnw core view document)
+  #:use-module (nnw core view)
   #:use-module (oop goops)
   #:use-module (uuid generate)
   #:export (<document>))
 
-;; Todo 
-(define-class <document> (<view>))
+;; TODO 实现document视图，document视图只是一个简单的，content的值为排序索引数值的视图，其view->string方法输出的是按索引顺序排版的block或view文本，要实现这个，可能需要单独实现一个根据id判定元素是block还是view的方法。
+(define-class <document> (<view>)
+  (type #:init-value "document"))
