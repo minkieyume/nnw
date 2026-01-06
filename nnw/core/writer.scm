717388dfaffd814a70a2e4961f7ccdb60e9b618e
MinkieYume <sign@yumieko.com>
2026-01-06 15:41:01 +0800

    refactor: rename module and add TODO comment for write method

--- nnw/core/writer.scm
+++ nnw/core/writer.scm
@@ -1,15 +1,8 @@
-(define-module (nnw core parser)
+(define-module (nnw core writer)
   #:use-module (nnw core generic)
   #:use-module (nnw core utils)
   #:use-module (nnw core view)
   #:use-module (oop goops)
   #:export ())
 
-(define example-output-sxml
-  '(view (@ (id "xxxx-xxxx-xxxxx-xxxx(uuidv4)")
-	    (type "document")
-	    (name "the view")
-	    ;; ... metadata
-	    )
-	 (block (@ (id "xxxx-xxxx-xxxxx-xxxx(uuidv4)")))
-	 (view (@ (id "xxxx-xxxx-xxxxx-xxxx(uuidv4)")))))
+;; TODO 完成'write->text'方法，以通过'test/writer.scm'的测试。
